39caba77c5d8e9cbe319965daf456473c51d77cc
devarrowsmith <devarrowsmith@gmail.com>
2021-06-29 20:11:40 +0100

    Update promise-katas-1 to pass the test 'theNumberOfTheBeast'.

--- src/promise-katas-1.js
+++ src/promise-katas-1.js
@@ -79,8 +79,6 @@ const returnList = () => {
     })
 };
 
-
-
 // 6 Create a function that returns a promise which returns "An error occurred" if it rejects
 // Example:
 // anError() rejects to "An error occurred"
@@ -95,7 +93,11 @@ const anError = () => {
 // Example:
 // theNumberOfTheBeast() rejects to the value 666
 
-const theNumberOfTheBeast = () => { };
+const theNumberOfTheBeast = () => {
+    return new Promise((resolve, reject) => {
+        reject(666)
+    })
+};
 
 // 8 Create a function that returns a promise which returns an object with a key of error and value of 500 if it rejects
 // Example:
